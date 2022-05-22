8502ff4de0c93cb103be864c9dbfffc85c2f41ab
yishai <yishai5555@gmail.com>
2022-05-22 22:36:21 +0300

    05-foreign-key-and-print-created-customer

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const sequelize = require('./util/database');
 const Customer = require('./models/customer');
 const Order = require('./models/order');
 
+Customer.hasMany(Order);
+
 sequelize
   .sync({ force: true })
   .then((result) => {
     return Customer.create({ name: 'Chandler Bing', email: 'cb@gmail.com' });
-    console.log(result);
+    // console.log(result);
+  })
+  .then((customer) => {
+    console.log({ customer });
   })
   .catch((err) => {
     console.log(err);

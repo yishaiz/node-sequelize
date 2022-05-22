7365a6b4740c69be689a8dc1106dd0bb6c764934
yishai <yishai5555@gmail.com>
2022-05-22 22:05:10 +0300

    04-catch-error

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const sequelize = require('./util/database');
 const Customer = require('./models/customer');
 const Order = require('./models/order');
 
-sequelize.sync({ force: true }).then((result) => {
-  return Customer.create({ name: 'Chandler Bing', email: 'cb@gmail.com' });
-  console.log(result);
-});
+sequelize
+  .sync({ force: true })
+  .then((result) => {
+    return Customer.create({ name: 'Chandler Bing', email: 'cb@gmail.com' });
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

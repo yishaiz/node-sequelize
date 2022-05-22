fd8ef4bcdafdeb8cb6de0caed9dc38fabd69235f
yishai <yishai5555@gmail.com>
2022-05-22 19:13:44 +0300

    01

---
+++ util/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+const db = 'db';
+const user = 'root';
+const password = '12345';
+const host = 'localhost';
+
+const sequelize = new Sequelize(db, user, password, {
+  dialect: 'mysql',
+  host,
+});
+
+module.exports = sequelize;

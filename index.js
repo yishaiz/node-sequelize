4847e28c869a46cddd98780650d2623762cdb07d
yishai <yishai5555@gmail.com>
2022-05-22 23:00:43 +0300

    07-get-customer-orders

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ const Order = require('./models/order');
 
 Customer.hasMany(Order);
 
+let customerId = null;
+
 sequelize
   .sync({ force: true })
   .then((result) => {
     return Customer.create({ name: 'Chandler Bing', email: 'cb@gmail.com' });
   })
   .then((customer) => {
+    customerId = customer.id;
     console.log({ customer });
-    return Order.create({ total: 45 });
+    return customer.createOrder({ total: 45 });
   })
   .then((order) => {
     console.log({ order });
+    return Order.findAll({ where: customerId });
+  })
+  .then((orders) => {
+    console.log('All the Orders are : ', orders);
   })
   .catch((err) => {
     console.log(err);
